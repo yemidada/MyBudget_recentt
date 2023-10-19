7a65f23cf9c7a173a93e6b266f5fe0892481d259
Adeyemi Dada <yemidada@gmail.com>
2023-10-19 21:55:28 +0100

    Resolved database error

--- db/migrate/20231017102212_create_entities.rb
+++ db/migrate/20231017102212_create_entities.rb
@@ -3,7 +3,6 @@ class CreateEntities < ActiveRecord::Migration[7.1]
     create_table :entities do |t|
       t.string :name
       t.decimal :amount
-      t.integer :author_id
       t.integer :group_id
       t.timestamps
     end
